4a73dcd03986d79f869f2688bea5abcfbf348542
yuxxnaxxxx <geyunagi@gmail.com>
2024-04-20 21:32:52 +0900

    fix: コメントを削除

--- 02.calendar/cal.rb
+++ 02.calendar/cal.rb
@@ -28,23 +28,18 @@ class Calendar
   end
 
   def print_calendar
-    # 渡された年月が正しいかチェックする
     valid_year
     valid_month
 
-    # 対象月の初日、最終日を取得する
     first_day = Date.new(@year, @month, 1)
     last_day = Date.new(@year, @month, -1)
 
-    # 月の名前を取得する
     time = Time.new(@year, @month)
     month_name = time.strftime("%B")
     
-    # 初週の表示位置を調整する
     first_day_weekday = first_day.wday
     blank =  "   " * first_day_weekday
 
-    # カレンダーを出力する
     puts "#{month_name} #{@year}".center(20)
     puts "Su Mo Tu We Th Fr Sa"
     print blank if first_day_weekday != 0
@@ -62,16 +57,13 @@ class Calendar
   end
 end
 
-# optparseで引数を受け取れるようにする
 params = {}
 opt = OptionParser.new
 opt.on('-y year', Integer) {|year| params[:year] = year }
 opt.on('-m month', Integer) {|month| params[:month] = month }
 opt.parse!(ARGV)
 
-# 現在の日付を取得
 current_day = Date.today
-# 引数が指定されていない場合は、現在の月, 年を取得
 year = params[:year] || current_day.year
 month = params[:month] || current_day.month
 
