7e6c1506a548e1daa6c4651ab775e9802008fb11
yuxxnaxxxx <geyunagi@gmail.com>
2024-05-05 15:12:26 +0900

    fix: eachを使って処理するように修正

--- 03.bowling/bowling.rb
+++ 03.bowling/bowling.rb
@@ -1,10 +1,12 @@
-# frozen_string_literal: true
-
 # !/usr/bin/env ruby
+
+# frozen_string_literal: true
 
 score = ARGV[0]
 scores = score.split(',')
 shots = []
+
+# スコアを数値に変換する
 scores.each do |s|
   if s == 'X'
     shots << 10
@@ -20,32 +22,20 @@ shots.each_slice(2) do |shot|
 end
 
 point = 0
-for i in 0..8 # 10フレーム目のみ処理が異なるため、9フレーム目までくり返す
-  if frames[i][0] == 10 # ストライク
-    point += 10
-    if frames[i + 1][0] == 10
-      point += frames[i + 1][0]
-      point += frames[i + 2][0]
-    else
-      point += (frames[i + 1]).sum # 次のフレームの合計スコア
-    end
-  elsif frames[i][0] < 10 && frames[i].sum == 10 # スペア
-    point += frames[i].sum
-    point += frames[i + 1][0]
-  else
-    point += frames[i].sum
-  end
+frames.each_with_index do |frame, index|
+  point += if frame[0] == 10
+             if index >= 9
+               10
+             elsif frames[index + 1][0] == 10
+               0 + frames[index + 1][0] + frames[index + 2][0]
+             else
+               10 + (frames[index + 1]).sum
+             end
+           elsif index < 9 && frames[index].sum == 10
+             frames[index].sum + frames[index + 1][0]
+           else
+             frames[index].sum
+           end
 end
-
-last_frame = []
-i = 9 # 10フレーム目の処理を対応するため、index を9で指定する
-while i < frames.count
-  last_frame << frames[i][0]
-  last_frame << frames[i][1]
-  i += 1
-end
-
-last_frame_score = last_frame.compact.sum
-point += last_frame_score
 
 puts point
