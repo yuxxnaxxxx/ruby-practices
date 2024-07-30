02ec2cd20d218328394175099bdbb8251b5bc566
yuxxnaxxxx <geyunagi@gmail.com>
2024-07-30 19:48:43 +0900

    fix: 変数名、メソッド名の修正、処理の修正

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -5,7 +5,7 @@
 COLUMN_WIDTH = 8
 DEFAULT_COLUMN = 3
 
-def get_files(include_hidden: false)
+def get_filenames(include_hidden: false)
   all_files = Dir.entries('.')
   files = if include_hidden
             all_files
@@ -15,48 +15,48 @@ def get_files(include_hidden: false)
   files.sort
 end
 
-def get_max_file_name_length(files)
-  return 0 if files.empty?
+def get_max_filename_length(filenames)
+  return 0 if filenames.empty?
 
-  files.map(&:size).max
+  filenames.map(&:size).max
 end
 
-def ordered_files(files, columns = DEFAULT_COLUMN)
+def convert_file_order(files, columns = DEFAULT_COLUMN)
   file_count = files.count.to_f
   rows = (file_count / columns).ceil
 
-  order_files = []
-  rows.times { order_files << [] }
+  ordered_files = []
+  rows.times { ordered_files << [] }
 
   files.each_with_index do |file, i|
-    order_files[i % rows] << file
+    ordered_files[i % rows] << file
   end
 
-  order_files
+  ordered_files
 end
 
-def get_column_width(max_file_name_length)
-  if (max_file_name_length % COLUMN_WIDTH).zero?
-    COLUMN_WIDTH * 2
-  elsif max_file_name_length > COLUMN_WIDTH
-    (max_file_name_length.to_f / COLUMN_WIDTH).ceil * COLUMN_WIDTH
+def get_column_width(max_filename_length)
+  if (max_filename_length % COLUMN_WIDTH).zero?
+    max_filename_length + COLUMN_WIDTH
+  elsif max_filename_length > COLUMN_WIDTH
+    (max_filename_length.to_f / COLUMN_WIDTH).ceil * COLUMN_WIDTH
   else
     COLUMN_WIDTH
   end
 end
 
 def output_file_name(files, columns = DEFAULT_COLUMN)
-  max_file_name_length = get_max_file_name_length(files)
-  width = get_column_width(max_file_name_length)
-  ordered_files_result = ordered_files(files, columns)
+  max_filename_length = get_max_filename_length(files)
+  width = get_column_width(max_filename_length)
+  ordered_files_result = convert_file_order(files, columns)
 
   ordered_files_result.each do |file|
     file.each do |name|
       print name.ljust(width, ' ')
     end
-    print "\n"
+    puts
   end
 end
 
-files = get_files
+files = get_filenames
 output_file_name(files, DEFAULT_COLUMN)
